e0a058a48105e795e2b8096a48d37ec150827d4a
Ignat Polnyakov <ignat-88@bk.ru>
2022-03-27 10:43:49 +0300

    Add registration part

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Ignat Polnyakov'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,6 +27,28 @@ $user_name = ''; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
+
+  <?php if ($is_auth == 1)
+  echo <<<HERE
+  <div class="user-menu__logged">
+  <p>Ignat Polnyakov</p>
+  <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+  <a class="user-menu__logout" href="#">Выход</a>
+</div>
+HERE;
+  else
+echo <<<HERE
+<ul class="user-menu__list">
+  <li class="user-menu__item">
+    <a href="#">Регистрация</a>
+  </li>
+  <li class="user-menu__item">
+    <a href="#">Вход</a>
+  </li>
+</ul>
+HERE;
+?>
+
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
