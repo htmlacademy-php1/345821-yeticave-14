2ac8782414a5b826161231ec513782430beda710
Ignat Polnyakov <ignat-88@bk.ru>
2022-03-30 03:32:33 +0300

    Done

--- index.php
+++ index.php
@@ -28,26 +28,23 @@ $user_name = 'Ignat Polnyakov'; // укажите здесь ваше имя
 
         <nav class="user-menu">
 
-  <?php if ($is_auth == 1)
-  echo <<<HERE
-  <div class="user-menu__logged">
-  <p>Ignat Polnyakov</p>
-  <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-  <a class="user-menu__logout" href="#">Выход</a>
-</div>
-HERE;
-  else
-echo <<<HERE
-<ul class="user-menu__list">
-  <li class="user-menu__item">
-    <a href="#">Регистрация</a>
-  </li>
-  <li class="user-menu__item">
-    <a href="#">Вход</a>
-  </li>
-</ul>
-HERE;
-?>
+          <?php if ($is_auth == 1) : ?>
+            <div class="user-menu__logged">
+              <?php $user_name = 'Ignat Polnyakov'; ?>
+            <p><?=$user_name; ?></p>
+            <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+            <a class="user-menu__logout" href="#">Выход</a>
+          </div>
+          <?php  else : ?>
+          <ul class="user-menu__list">
+            <li class="user-menu__item">
+              <a href="#">Регистрация</a>
+            </li>
+            <li class="user-menu__item">
+              <a href="#">Вход</a>
+            </li>
+          </ul>
+          <?php endif; ?>
 
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
