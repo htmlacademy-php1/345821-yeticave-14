06a7aa55b06257bf9459e95da1ac5c7eedde9757
Ignat Polnyakov <ignat-88@bk.ru>
2022-04-11 11:47:15 +0300

    1

--- index.php
+++ index.php
@@ -75,7 +75,9 @@ $numbers = ceil($number);
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+
     <meta charset="UTF-8">
+    <title>Главная</title>
     <link href="../css/normalize.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
 </head>
