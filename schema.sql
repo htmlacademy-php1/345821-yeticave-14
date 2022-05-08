7171e6bb75d7a877ca644d9e2f28313a3f57bf8b
Ignat Polnyakov <ignat-88@bk.ru>
2022-05-08 16:41:49 +0300

    done

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE yeticave
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+
 USE yeticave;
 
 CREATE TABLE categories(
@@ -12,24 +13,31 @@ CREATE TABLE categories(
     id INT AUTO_INCREMENT PRIMARY KEY,
     dt_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(64) NOT NULL UNIQUE,
-    name VARCHAR(90) NOT NULL UNIQUE,
+    name VARCHAR(90) NOT NULL ,
     password VARCHAR(50) NOT NULL,
-    contact VARCHAR(128) NOT NULL UNIQUE
+    contact VARCHAR(128) NOT NULL UNIQUE,
+    bet_id INT NOT NULL ,
+    lot_id INT NOT NULL
   );
 
     CREATE TABLE lots(
       id INT AUTO_INCREMENT PRIMARY KEY,
       dt_lot TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      name VARCHAR(64) NOT NULL UNIQUE,
-      descriprion VARCHAR(220) NOT NULL UNIQUE,
+      name VARCHAR(64) NOT NULL,
+      descriprion VARCHAR(220) NOT NULL,
       img_link VARCHAR(300) NOT NULL,
       start_price INT NOT NULL,
       dt_end DATETIME,
-      up_bet INT NOT NULL
+      up_bet INT NOT NULL,
+      cat_id INT NOT NULL ,
+      us_id INT NOT NULL
     );
 
 
       CREATE TABLE bet(
         id INT AUTO_INCREMENT PRIMARY KEY,
         dt_bet TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        bet_price INT NOT NULL);
+        bet_price INT NOT NULL,
+        us_id INT NOT NULL ,
+        lot_id INT NOT NULL
+      );
