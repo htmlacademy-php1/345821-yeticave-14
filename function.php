b45008d79e0c39180276b9cba0c74262ff5c7310
Ignat Polnyakov <ignat-88@bk.ru>
2022-05-16 19:47:06 +0300

    update

--- function.php
+++ function.php
@@ -1,29 +1,40 @@
 <?php
- date_default_timezone_set("Europe/Moscow");
-function retail_price($number) {
-   $numbers = ceil($number);
+
+date_default_timezone_set("Europe/Moscow");
+
+function retail_price(int $number)
+{
+    $numbers = ceil($number);
+    $total_number = number_format($numbers, 0, '', ' ');
+
     if ($numbers < 1000) {
-          $total_number = $numbers;
-        } else  {
-          $total_number = number_format($numbers, 0,'', ' ');
-        }
-    return $total_number .' ₽';
+        $total_number = $numbers;
+    }
+
+    return $total_number . ' ₽';
 }
 
- function expired_time($val){
+function expired_time(string $val)
+{
     $total_end = strtotime($val);
     $dt_now = time();
-    $total_time = $total_end-$dt_now;
+    $total_time = $total_end - $dt_now;
     $hours = floor($total_time / 3600);
-    $minutes = floor(($total_time % 3600)/60);
- return "$hours : $minutes" ;
+    $minutes = floor(($total_time % 3600) / 60);
+
+    return $hours . ' : ' . $minutes;
 }
 
-function view_class($val){
-   $total_end = strtotime($val);
-   $dt_now = time();
-   $total_time = $total_end-$dt_now;
- if ($total_time < 3600) {
-   return 'timer--finishing';}
+function view_class(string $val)
+{
+    $result = '';
+    $total_end = strtotime($val);
+    $dt_now = time();
+    $total_time = $total_end - $dt_now;
+
+    if ($total_time < 3600) {
+        $result = 'timer--finishing';
+    }
+
+    return $result;
 }
- ?>
--- queries.sql
+++ queries.sql
@@ -1,38 +1,48 @@
 USE yeticave; -- подключение к базе данных
-INSERT INTO categories (cat_id)
-VALUES ('boards'),('attachment'),('boots'),('clothing'),('tools'),('other') ;
-INSERT INTO categories (cat_name)
-VALUES (' Доски и лыжи'),('Крепления'),('Ботинки'),('Одежда'),('Инструменты'),('Разное');
+
+INSERT INTO categories (title, name)
+VALUES ('boards',' Доски и лыжи'),
+('attachment','Крепления'),
+('boots','Ботинки'),
+('clothing','Одежда'),
+('tools','Инструменты'),
+('other','Разное') ;
 -- заполнение таблицы категорий
 
-INSERT INTO users (dt_reg)
-VALUES (NOW); -- СПОРНЫЙ ВОПРОС С ДАТОЙ!
-INSERT INTO users(email)
-VALUES ('ignat-88@bk.ru'),('test@mail.ru');
-INSERT INTO users (name)
-VALUES (Ignat),(Petr);
-INSERT INTO users (paasword)
-VALUES (Qwerty15),(Asd18);
-INSERT INTO users (contact)
-VALUES ('89100136555'),('84952741001');
+INSERT INTO users(email, name, password, contact, bet_id, lot_id)
+VALUES ('ignat-88@bk.ru','Ignat','Qwerty15','89100136555','',''),
+('test@mail.ru','Petr','Asd18','84952741001','','');
 --заполнение таблицы пользователей
 
-INSERT INTO lots (dt_lot)
-VALUES (NOW); --спорный вопрос
-INSERT INTO lots (name),
-VALUES ('2014 Rossignol District Snowboard'),('DC Ply Mens 2016/2017 Snowboard'),('Крепления Union Contact Pro 2015 года размер L/XL'),('Ботинки для сноуборда DC Mutiny Charocal'),('Куртка для сноуборда DC Mutiny Charocal'),('Маска Oakley Canopy');ABS
-INSERT INTO lot (descriprion),
-VALUES ;
-INSERT INTO lots (img_link),
-VALUES ('img/lot-1.jpg'), ('img/lot-2.jpg'),('img/lot-3.jpg'),('img/lot-4.jpg'),('img/lot-5.jpg'),('img/lot-6.jpg');
-INSERT INTO lots (start_price)
-VALUES (10999),(159999),(8000),(10999),(7500),(5400);
-INSERT INTO lots (dt_end)
-VALUES ("2022-05-23"),("2022-04-25"),("2022-05-04"),("2022-05-01"),("2022-04-30"),("2022-05-10");
-INSERT INTO lots (up_bet)
-VALUES (500),(500),(500),(500),(500),(500);
+INSERT INTO lots (name, descriprion, img_link, start_price, end_date, bet_step,created_by, category_id, win_user_id),
+VALUES ('2014 Rossignol District Snowboard','сноуборд','img/lot-1.jpg', '10999','2022-05-23', '500','1' '1','2'),
+('DC Ply Mens 2016/2017 Snowboard','сноуборд','img/lot-2.jpg', '159999','2022-04-25', '500','2' ,'1', '1'),
+('Крепления Union Contact Pro 2015 года размер L/XL','крепление новое', 'img/lot-3.jpg', '8000','2022-05-04','500','2', '2', '1'),
+('Ботинки для сноуборда DC Mutiny Charocal','img/lot-4.jpg','10999','2022-05-01','500','1' '3', '2'),
+('Куртка для сноуборда DC Mutiny Charocal','img/lot-5.jpg','7500','2022-04-30','500','1' '4' , '2'),
+('Маска Oakley Canopy','img/lot-6.jpg','5400','2022-05-10','500','2' '6', '1');
 --заполнение таблицы объявлений
 
+INSERT INTO bet (price, user_id, lot_id)
+VALUES ('8000', '1','5'),
+('8500', '2', '3');--заполнение таблицы ставок
 
+SELECT * FROM catogories; -- получить все категории
 
-SELECT cat_name FROM catogories;
+SELECT l.name, l.start_price, l.img_link, MAX(b.price) AS max_price, c.name
+FROM lots l
+JOIN bet b ON l.id = b.lot_id
+JOIN categories c ON l.category_id = c.id
+WHERE l.end_date > "2022.05.16 17:50:00"; --получить список открытых лотов
+
+SELECT name, descriprion, img_link, start_price, end_date, bet_step, created_by, c.name
+FROM lots
+JOIN categories c ON category_id = c.id
+WHERE id = 2; --показать лот по его ID
+
+UPDATE lots SET name = 'DC Ply Mens 2017/2018 Snowboard' WHERE id = 2; --обновление название лота
+
+SELECT b.price,
+FROM bet b
+WHERE id = 5
+ORDER BY b.created_at DESC ;--получить список ставок для лота по его идентификатору с сортировкой по дате.
