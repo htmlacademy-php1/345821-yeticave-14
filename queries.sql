9f6cee6d48b235a336f0fbf067eff840928b0d2f
Ignat Polnyakov <ignat-88@bk.ru>
2022-05-18 15:54:00 +0300

    complete

--- queries.sql
+++ queries.sql
@@ -16,10 +16,10 @@ VALUES ('ignat-88@bk.ru','Ignat','Qwerty15','89100136555'),
 
 INSERT INTO lots (name, descriprion, img_link, start_price, end_date, bet_step,created_by, category_id, win_user_id)
 VALUES ('2014 Rossignol District Snowboard', 'сноуборд','img/lot-1.jpg','10999','2022-05-23', '500','1','1','2'),
-('DC Ply Mens 2016/2017 Snowboard','сноуборд','img/lot-2.jpg', '159999','2022-04-25', '500','2' ,'1','1'),
-('Крепления Union Contact Pro 2015 года размер L/XL','крепление новое', 'img/lot-3.jpg', '8000','2022-05-04','500','2', '2', '1'),
-('Ботинки для сноуборда DC Mutiny Charocal','боты удобные фиксируют стопу','img/lot-4.jpg','10999','2022-05-01','500','1','3', '2'),
-('Куртка для сноуборда DC Mutiny Charocal','куртка класс','img/lot-5.jpg','7500','2022-04-30','500','1', '4', '2'),
+('DC Ply Mens 2016/2017 Snowboard','сноуборд','img/lot-2.jpg', '159999','2022-05-25', '500','2' ,'1','1'),
+('Крепления Union Contact Pro 2015 года размер L/XL','крепление новое', 'img/lot-3.jpg', '8000','2022-05-30','500','2', '2', '1'),
+('Ботинки для сноуборда DC Mutiny Charocal','боты удобные фиксируют стопу','img/lot-4.jpg','10999','2022-06-01','500','1','3', '2'),
+('Куртка для сноуборда DC Mutiny Charocal','куртка класс','img/lot-5.jpg','7500','2022-05-29','500','1', '4', '2'),
 ('Маска Oakley Canopy','маска защищает от ветра и снега','img/lot-6.jpg','5400','2022-05-10','500','2','6','1');
 --заполнение таблицы объявлений
 
@@ -33,16 +33,18 @@ SELECT l.name, l.start_price, l.img_link, MAX(b.price) AS max_price, c.name
 FROM lots l
 JOIN bet b ON l.id = b.lot_id
 JOIN categories c ON l.category_id = c.id
-WHERE l.end_date > "2022.05.17 11:40:00"; --получить список открытых лотов
+WHERE l.end_date > NOW()
+GROUP BY l.id; --получить список открытых лотов
 
-SELECT name, descriprion, img_link, start_price, end_date, bet_step, created_by, c.name
-FROM lots
+SELECT l.name, descriprion, img_link, start_price, end_date, bet_step, created_by, c.name
+FROM lots l
 JOIN categories c ON category_id = c.id
-WHERE id = 2; --показать лот по его ID
+WHERE l.id = 2
+--показать лот по его ID
 
 UPDATE lots SET name = 'DC Ply Mens 2017/2018 Snowboard' WHERE id = 2; --обновление название лота
 
-SELECT b.price,
+SELECT b.price
 FROM bet b
 WHERE b.lot_id = 5
 ORDER BY b.created_at DESC ;--получить список ставок для лота по его идентификатору с сортировкой по дате.
