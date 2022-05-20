94963dd14396875cd30ea7ca0a667c0a7503f7af
Ignat Polnyakov <ignat-88@bk.ru>
2022-05-20 16:31:10 +0300

    up

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!$link) {
 }
 else {
   // запрос на получение списка категорий
-  $sql = 'SELECT id, name, title FROM categories';
+  $sql = 'SELECT id, name, title FROM categories ';
   $result = mysqli_query($link, $sql);
 
     if ($result){
@@ -22,12 +22,12 @@ else {
   }
 
   // запрос на показ лотов
-   $sql = 'SELECT l.name, l.start_price, l.img_link, MAX(b.price) AS max_price, c.name FROM lots l'
-   . 'LEFT JOIN bet b ON l.id = b.lot_id'
-   . 'JOIN categories c ON l.category_id = c.id'
-   . 'WHERE l.end_date > NOW()'
-   . 'GROUP BY l.id'
-   . 'ORDER BY l.сreated_at DESC LIMIT 6';
+   $sql = 'SELECT l.name, l.start_price, l.img_link, MAX(b.price) AS max_price, c.name FROM lots l '
+   . 'LEFT JOIN bet b ON l.id = b.lot_id '
+   . 'JOIN categories c ON l.category_id = c.id '
+   . 'WHERE l.end_date > NOW() '
+   . 'GROUP BY l.id '
+   . 'ORDER BY l.сreated_at DESC LIMIT 6 ';
 
    if ($res = mysqli_query($link, $sql)){
       $lots = mysqli_fetch_all($res, mode: MYSQLI_ASSOC);
